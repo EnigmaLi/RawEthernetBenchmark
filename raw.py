57e34f3d50917f48686a45488039ce3f48498a71
Zhilong Li <lizhilong.enigma@gmail.com>
2016-07-11 09:47:45 -0400

    test v0.1

--- raw.py
+++ raw.py
@@ -3,7 +3,7 @@
 import sys
 import socket
 
-def send_eth(src, dst, eth_type, payload, interface = "enp0s3"):
+def send_eth(src, dst, eth_type, payload, interface = "eth1"):
   """Send raw Ethernet packet on interface."""
 
   assert(len(src) == len(dst) == 6) # 48-bit ethernet addresses
@@ -17,7 +17,7 @@ def send_eth(src, dst, eth_type, payload, interface = "enp0s3"):
   s.send((src + dst + eth_type + payload).encode("utf-8"))
   s.close()
 
-def recv_eth(buff_size, interface = "enp0s3"):
+def recv_eth(buff_size, interface = "eth1"):
 
     # create a raw socket and bind it to the public interface
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     if(sys.argv[1] == "client"):
 
         while(True):
-            send_eth("\x08\x00\x27\x08\xDE\x43", "\xA4\x5E\x60\xF4\x09\x1F", "\x00\x05", "hello")
+            #send_eth("\x08\x00\x27\x08\xDE\x43", "\xA4\x5E\x60\xF4\x09\x1F", "\x00\x05", "hello")
             #send_eth("\x08\x00\x27\x08\xDE\x43", "\x08\x00\x27\x08\xDE\x43", "\x08\x00", "hello")
-
+			send_eth("\x00\x02\xC9\x4D\x45\xC8", "\xF4\x52\x14\x94\x99\x60", "\x00\xE5", "hello")
 
 
     elif(sys.argv[1] == "server"):
