ae663d3898b81359a7a24853f887d425218b2112
Zhilong Li <lizhilong.enigma@gmail.com>
2016-07-11 09:57:20 -0400

    Test

--- raw.py
+++ raw.py
@@ -11,8 +11,8 @@ def send_eth(src, dst, eth_type, payload, interface = "eth1"):
 
   s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
 
-  # From the docs: "For raw packet
-  # sockets the address is a tuple (ifname, proto [,pkttype [,hatype]])"
+  ## From the docs: "For raw packet sockets the address is a tuple:
+  ## (ifname, proto [,pkttype [,hatype]])
   s.bind((interface, 0))
   s.send(src + dst + eth_type + payload)
   s.close()
@@ -21,7 +21,7 @@ def recv_eth(buff_size, interface = "eth1"):
 
     # create a raw socket and bind it to the public interface
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    s.bind((interface, 0x00E5))
+    s.bind((interface, 0x809B))
 
     # receive a package
     print(s.recvfrom(buff_size))
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         while(True):
             #send_eth("\x08\x00\x27\x08\xDE\x43", "\xA4\x5E\x60\xF4\x09\x1F", "\x00\x05", "hello")
             #send_eth("\x08\x00\x27\x08\xDE\x43", "\x08\x00\x27\x08\xDE\x43", "\x08\x00", "hello")
-			send_eth("\x00\x02\xC9\x4D\x45\xC8", "\xF4\x52\x14\x94\x99\x60", "\x00\xE5", "hello")
+			send_eth("\x00\x02\xC9\x4D\x45\xC8", "\xF4\x52\x14\x94\x99\x60", "\x80\x9B", "hello")
 
 
     elif(sys.argv[1] == "server"):
